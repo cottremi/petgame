619e620114e0d7a3581ef87472ba5c787d7140c5
Katie <katie.cottrell@ucdenver.edu>
2020-04-09 12:22:43 -0600

    My Second Commit

---
+++ Assets/Scripts/MaterialEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MaterialEditor : MonoBehaviour
+{
+
+    MeshRenderer meshRenderer;
+    public Texture albedoTexture1;
+    public Texture albedoTexture2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.SetTexture("_MainTex", albedoTexture1);
+    }
+}
---
+++ Assets/Scripts/MouseInput.cs
@@ -0,0 +1,47 @@
+﻿// ADAPTED FROM TUTORIAL: https://www.youtube.com/watch?v=EANtTI6BCxk 
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    public string objectBeingPet = "none";
+
+    void Start()
+    {
+       
+    }
+    
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(0)) {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(hit.transform != null)
+                {
+                    //PrintName(hit.transform.gameObject);
+                    objectBeingPet = (hit.transform.gameObject.name);
+                    print(objectBeingPet);
+              
+                } 
+            }
+	    }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            objectBeingPet = "none";
+            print(objectBeingPet);
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+}
